b0a201fb0ddf41e34c7e036e01771eb0c5ffac46
yiyione <yiyi_one@foxmail.com>
2020-05-29 04:17:56 +0800

    test

--- client/src/components/Boss.vue
+++ client/src/components/Boss.vue
@@ -3,24 +3,22 @@
     <v-divider inset></v-divider>
     <h1>BOSS状态</h1>
     <v-row>
-      <v-col :span="12"></v-col>
-      <v-col :span="12"><a class="exbig">{{ bossData.round }}</a><a class="big">周目</a></v-col>
-      <v-col :span="12"><a class="exbig">{{ bossData.boss }}</a><a class="big">号boss</a></v-col>
-      <v-col :span="12"></v-col>
+      <v-col col="6"><a class="exbig">{{ bossData.round }}</a><a class="big">周目</a></v-col>
+      <v-col col="6"><a class="exbig">{{ bossData.boss }}</a><a class="big">号boss</a></v-col>
     </v-row>
     <v-row>
-      <v-col :span="24"><span :style="{'color':getProgressLinearColor()}" :class="{ exbig: !bossData.challenger }">{{ bossData.remind_hp.toLocaleString() }}</span>/{{ bossData.total_hp.toLocaleString() }}</v-col>
+      <v-col col="12"><span :style="{'color':getProgressLinearColor()}" :class="{ exbig: !bossData.challenger }">{{ bossData.remind_hp.toLocaleString() }}</span>/{{ bossData.total_hp.toLocaleString() }}</v-col>
     </v-row>
     <template v-if="bossData.challenger">
       <v-row>
-          <v-col :span="24">
+          <v-col col="12">
               <a style="font-size:32px;color:#003300;">{{ bossData.challenger.name }}</a>
               <a v-if="bossData.challenger"> 正在挑战boss </a>
           </v-col>
       </v-row>
     </template>
     <v-row style="margin-bottom: 25px">
-        <v-col :span="24">
+        <v-col col="12">
             <v-progress-linear
               :value="bossData.remind_hp/bossData.total_hp*100"
               :color="getProgressLinearColor()"
@@ -30,37 +28,37 @@
     </v-row>
 
     <v-row justify="center">
-      <v-col cols="2">
+      <v-col>
         <v-btn x-large color="success" min-width="100%">报刀</v-btn>
       </v-col>
-      <v-col cols="2">
+      <v-col>
         <v-btn x-large color="warning" min-width="100%">补时刀</v-btn>
       </v-col>
-      <v-col cols="2">
+      <v-col>
         <v-btn x-large color="warning" min-width="100%">尾刀</v-btn>
       </v-col>
     </v-row>
     <v-row justify="center">
-      <v-col cols="2">
+      <v-col>
         <v-btn x-large color="error" min-width="100%">掉刀</v-btn>
       </v-col>
-      <v-col cols="2">
+      <v-col>
         <v-btn x-large color="error" min-width="100%">挂树</v-btn>
       </v-col>
     </v-row>
     <v-row justify="center">
-      <v-col cols="2">
+      <v-col>
         <v-btn x-large color="primary" min-width="100%"> 申请出刀 </v-btn>
       </v-col>
-      <v-col cols="2">
+      <v-col>
         <v-btn x-large min-width="100%"> 取消申请 </v-btn>
       </v-col>
     </v-row>
     <v-row justify="center">
-      <v-col cols="2">
+      <v-col>
         <v-btn x-large min-width="100%">预约BOSS</v-btn>
       </v-col>
-      <v-col cols="2">
+      <v-col>
         <v-btn x-large min-width="100%"> 取消预约 </v-btn>
       </v-col>
     </v-row>
@@ -112,15 +110,12 @@ export default {
     text-align:center;
   }
   .big{
-    font-size:32px;
+    font-size:30px;
   }
   .exbig{
-    font-size:48px;
+    font-size:46px;
   }
   .v-row {
     margin-bottom: 15px;
-  }
-  v-button {
-    width: 80px;
   }
 </style>
