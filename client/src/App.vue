26d603a6d7c201ccc4eea909e1dc03f0b678f28e
yiyione <yiyi_one@foxmail.com>
2020-06-27 04:51:23 +0800

    Untracked files issue resolved to fix .gitignore

--- client/src/App.vue
+++ client/src/App.vue
@@ -19,7 +19,7 @@
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://game.bilibili.com/pcr/"
+        @click.stop="loginDialog = true"
         target="_blank"
         text
       >
@@ -28,6 +28,41 @@
       </v-btn>
     </v-app-bar>
 
+    <v-dialog v-model="loginDialog" max-width="350">
+      <v-card>
+        <v-toolbar
+          color="primary"
+          dark
+          flat
+        >
+          <v-toolbar-title>请输入群号和Token</v-toolbar-title>
+          <v-spacer></v-spacer>
+        </v-toolbar>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              label="群号"
+              name="groupId"
+              prepend-icon="mdi-account-multiple"
+              type="text"
+            ></v-text-field>
+
+            <v-text-field
+              id="token"
+              label="Token"
+              name="token"
+              prepend-icon="mdi-lock"
+              type="password"
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" @click="login()">登录</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
     <v-content>
       <Welcome/>
     </v-content>
@@ -35,7 +70,13 @@
 </template>
 
 <script>
+import Vue from 'vue';
+import VueCookies from 'vue-cookies';
+import axios from 'axios';
+
 import Welcome from './components/Welcome';
+
+Vue.use(VueCookies);
 
 export default {
   name: 'App',
@@ -45,10 +86,29 @@ export default {
   },
 
   data: () => ({
-    //
+    loginDialog: false,
+    hasLogin: false
   }),
   created() {
      document.title="公主连结·108集团";
+  },
+  methods: {
+    login(gid, token) {
+      axios.get(`/api/login?gid=${gid}`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }).then(res => {
+        Vue.$cookies.set('gid', gid);
+        Vue.$cookies.set('uid', res.uid);
+        this.hasLogin = true;
+      }).catch(err => {
+        console.log(err);
+      });
+    },
+    logout() {
+      this.hasLogin = false;
+    }
   }
 };
 </script>
